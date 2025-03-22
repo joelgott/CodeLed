4f1f13180f3c4d04627cb171bf052eb61f5427dc
joelgott <54689858+joelgott@users.noreply.github.com>
2025-03-21 21:22:47 -0300

    read and select randomly from txt file

--- code.py
+++ code.py
@@ -2,14 +2,24 @@ import asyncio
 import board
 from codeled import CodeLed
 import busio
+import random
 
-mensaje = "Cuantos dejarian de ser esclavos por el precio de una bomba al mar."
+try:
+    with open("/messages.txt", "r") as f:
+        messages = f.readlines()
+        #print(messages)
+        #message = messages[2]
+        message = random.choice(messages)
+        print("message chosen from filesystem")
+except OSError as e:
+    print("Internal message")
+    message = "Cuantos dejarian de ser esclavos por el precio de una bomba al mar."
 
-print(mensaje)
+print(message)
 
 async def main():
     morse = CodeLed(board.IO9, busio.I2C(scl = board.IO10, sda = board.IO11))
-    await morse.tx(code = "MORSE",msg = mensaje, t_ref = 200)
+    await morse.tx(code = "MORSE",msg = message, t_ref = 200)
 
 if __name__ == "__main__":
     asyncio.run(main())
